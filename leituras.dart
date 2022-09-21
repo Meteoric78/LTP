6de4eec73604ca60c86491884c4428d12cfec505
Meteoric78 <66694522+Meteoric78@users.noreply.github.com>
2022-09-21 15:38:56 -0300

    Add files via upload

---
+++ leituras.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+int lerInteiro({required String mensagem}) {
+  print(mensagem);
+  String? ler = stdin.readLineSync();
+
+  if (ler != null) {
+    int? value = int.tryParse(ler);
+
+    while (value == null) {
+      print(mensagem);
+      String? novamente = stdin.readLineSync();
+      if (novamente != null) value = int.tryParse(novamente);
+    }
+
+    return value;
+  }
+  return 0;
+}
