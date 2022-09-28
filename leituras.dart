273ea4afed22036d10ca48a5f68dfad1572ec87b
Meteoric78 <66694522+Meteoric78@users.noreply.github.com>
2022-09-28 16:06:07 -0300

    Update leituras.dart

--- leituras.dart
+++ leituras.dart
@@ -10,10 +10,15 @@ int lerInteiro({required String mensagem}) {
     while (value == null) {
       print(mensagem);
       String? novamente = stdin.readLineSync();
+
       if (novamente != null) value = int.tryParse(novamente);
     }
 
     return value;
   }
+
   return 0;
 }
+
+
+//Desafio para hoje a noite, analisar esta função, e resolver a mesma com do while. 
