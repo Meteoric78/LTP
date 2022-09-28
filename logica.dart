a4470adaccee1add30c5c9987d3f26bae26e0464
Meteoric78 <66694522+Meteoric78@users.noreply.github.com>
2022-09-28 16:06:48 -0300

    Update logica.dart

--- logica.dart
+++ logica.dart
@@ -1,44 +1,52 @@
 import 'dart:io';
 
+import 'package:aula3/leituras.dart' as leitura;
+import 'package:aula3/lista_vetor.dart' as lvet;
+
 int menu() {
-  print("Digite 1 para o primeiro exercício");
-  print("Digite 0 para sair");
-  String? aux = stdin.readLineSync();
-  if (aux != null) {
-    int? num = int.tryParse(aux);
-    return (num == null) ? -1 : num;
-  } else {
+  print(
+      "Digite  1 para ler dez elemetnos de um matriz unidimensional do tipo inteiro e apresentar os valores lidos");
+  print("Digite 0 para Sair");
+
+  String? ler = stdin.readLineSync();
+  if (ler != null) {
+    int? value = int.tryParse(ler);
+    if (value != null) {
+      return value;
+    } else
+      return -1;
+  } else
     return -1;
-  }
 }
 
 void inicio() {
-  int op;
+  int? op;
   do {
     op = menu();
     switch (op) {
       case 0:
         {
-          print("Obrigado por usar");
+          print("Obrigado por utilizar nosso aplicativo em terminal");
         }
         break;
       case 1:
         {
-          int i = 0;
-          while {
-            print("Insira 10 valores:");
-            String? input1 = stdin.readLineSync();
-            if (input1 != null) {
-              int? n
-            }
+          void imprimir(int v) {
+            if (v % 2 == 0) print(" O Elemento é $v   ");
           }
-          
+
+          ;
+
+          List<int> unidimensional = lvet.preencherLista(tamanho: 10);
+          //lvet.imprimirProceduralInteiro(lista: unidimensional);
+          //unidimensional.forEach(imprimir);
+          unidimensional.forEach((int e) {
+            if (e % 2 != 0) print("Elemento  é $e");
+          });
         }
         break;
       default:
-        {
-          print("Opção inválida");
-        }
+        print("Opção inválida");
     }
   } while (op != 0);
 }
