8e53779a7bb0853e3508350bd4679e6f64b7e358
Meteoric78 <66694522+Meteoric78@users.noreply.github.com>
2022-10-05 23:30:42 +0900

    Update leituras.dart

--- leituras.dart
+++ leituras.dart
@@ -1,24 +1,30 @@
 import 'dart:io';
 
 int lerInteiro({required String mensagem}) {
-  print(mensagem);
-  String? ler = stdin.readLineSync();
-
-  if (ler != null) {
-    int? value = int.tryParse(ler);
-
-    while (value == null) {
-      print(mensagem);
-      String? novamente = stdin.readLineSync();
-
-      if (novamente != null) value = int.tryParse(novamente);
+  String? input;
+  int? valor;
+  do {
+    print(mensagem);
+    input = stdin.readLineSync();
+    if (input != null) {
+      valor = int.tryParse(input);
     }
-
-    return value;
-  }
-
-  return 0;
+  } while (input == null || valor == null);
+  return valor;
 }
 
 
 //Desafio para hoje a noite, analisar esta função, e resolver a mesma com do while. 
+
+num lerNum({required String mensagem}) {
+  String? input;
+  num? valor;
+  do {
+    print(mensagem);
+    input = stdin.readLineSync();
+    if (input != null) {
+      valor = num.tryParse(input);
+    }
+  } while (input == null || valor == null);
+  return valor;
+}
