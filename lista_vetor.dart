fe299dcc8ce324dce8757eee12eb26a830c37ab6
Meteoric78 <66694522+Meteoric78@users.noreply.github.com>
2022-10-05 23:30:00 +0900

    Update lista_vetor.dart

--- lista_vetor.dart
+++ lista_vetor.dart
@@ -1,4 +1,5 @@
 import 'package:aula3/leituras.dart' as leitura;
+import 'package:projetinho3/leituras.dart' as leitura;
 
 List<int> preencherLista({required int tamanho}) {
   List<int> lista = [];
@@ -14,8 +15,48 @@ List<int> preencherLista({required int tamanho}) {
   return lista;
 }
 
-void imprimirProceduralInteiro({required List<int> lista}) {
-  for (int i = 0; i < lista.length; i++) {
-    print("O vetor na posicao [ $i ] = ${lista[i]}");
-  }
+List<int> produtoPorEscalar({required List<int> matrizA, int escalar = 3}) {
+  List<int> matrizB = [];
+  matrizA.forEach((e) {
+    matrizB.add(e * escalar);
+  });
+  return matrizB;
+}
+
+int somarElementos({required List<int> fonte, int inicio = 0, int fim = 0}) {
+  if (fim == 0) fim = fonte.length;  
+  var range = fonte.getRange(inicio, fim);
+  int valor = 0;
+  range.forEach((e) {
+    valor += e;
+  });
+  return valor;
+}
+
+double mediaArit({required List<int> matriz}) {
+  double media = somarElementos(fonte: matriz) / matriz.length.toDouble();
+  return media;
+}
+
+int menor({required List<int> matriz}) {
+  matriz.sort();
+  return matriz.first;
+}
+
+int maior({required List<int> matriz}) {
+  matriz.sort();
+  return matriz.last;
+}
+
+List<int> repeticoes({required List<num> matriz, required int a, required int b, required int c}) {
+  int a_rep = 0;
+  int b_rep = 0;
+  int c_rep = 0;
+  List<int> repetidos = [a_rep, b_rep, c_rep];
+  matriz.forEach((e) {
+    if (a == e) a_rep++;
+    if (b == e) b_rep++;
+    if (c == e) c_rep++;
+  });
+  return repetidos;
 }
